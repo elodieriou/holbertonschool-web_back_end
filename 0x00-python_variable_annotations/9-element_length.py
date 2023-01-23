6bdf90496a3305c463abe61d2ff72177b67ff26f
Elodie Riou <elodie.riou@hotmail.com>
2023-01-23 11:19:49 +0100

    FEATURE: type iterable and sequence

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module defines the function element_length.
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
