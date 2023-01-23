8eaa3f372a7113734aecd1a9bec00d6abc198e0d
Elodie Riou <elodie.riou@hotmail.com>
2023-01-23 09:56:01 +0100

    FEATURE: implement basic annotations on function floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module defines the function floor.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """This function returns the floor of a float number"""
+    return math.floor(n)
