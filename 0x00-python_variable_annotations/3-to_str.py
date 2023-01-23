cc9cef84a91de7751dc512cfc2a4788aa8ae6d24
Elodie Riou <elodie.riou@hotmail.com>
2023-01-23 10:01:00 +0100

    FEATURE: implement basic annotation on function to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module defines the function to_str.
+"""
+
+
+def to_str(n: float) -> str:
+    """This function return a float number onto str"""
+    return str(n)
