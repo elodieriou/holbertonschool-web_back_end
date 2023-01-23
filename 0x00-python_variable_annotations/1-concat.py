a47b6eca5554dd08a4a5d33c2b840368e63ea103
Elodie Riou <elodie.riou@hotmail.com>
2023-01-23 09:51:28 +0100

    FEATURE: implement basic annotations on fucntion concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module defines the function concat.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function returns the concatenation of two arguments str1 and str2"""
+    return str1 + str2
