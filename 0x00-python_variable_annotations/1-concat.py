4cacc8d7244e4416530a4ea0a8387fca11167dce
Elodie Riou <elodie.riou@hotmail.com>
2023-01-23 13:00:43 +0100

    FIX: max characters

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,5 +5,6 @@ This module defines the function concat.
 
 
 def concat(str1: str, str2: str) -> str:
-    """This function returns the concatenation of two arguments str1 and str2"""
+    """This function returns the concatenation of two arguments str1
+     and str2"""
     return str1 + str2
