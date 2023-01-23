819e1651e8978afdaedf0eefe7fca2a199d452e9
Elodie Riou <elodie.riou@hotmail.com>
2023-01-23 10:34:37 +0100

    FEATURE: types of list of float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module defines the function sum_list.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """This function returns the sum of all elements in the list"""
+    return sum(input_list)
