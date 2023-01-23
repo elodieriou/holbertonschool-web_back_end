e36f4a7a573939d3f4ca2937695cc41c9d3d19d4
Elodie Riou <elodie.riou@hotmail.com>
2023-01-23 09:42:23 +0100

    FEATURE: implement basic annotations on function add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module defines the function add.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """This function returns the addition of two arguments a and b"""
+    return a + b
