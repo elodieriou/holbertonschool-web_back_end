0365b85df587329f1c1d5475e01c41d0885627ac
Elodie Riou <elodie.riou@hotmail.com>
2023-01-23 10:04:07 +0100

    FEATURE: define type of variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module defines four variables with their annotations.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
