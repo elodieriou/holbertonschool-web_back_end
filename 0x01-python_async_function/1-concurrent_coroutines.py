7dc42b95fee2e416b4ba04fbbf33c55aea3b08fd
Elodie Riou <elodie.riou@hotmail.com>
2023-01-24 12:48:22 +0100

    FIX: comment

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """This method calls n time the method wait_random using
-    create_task function and then """
+    create_task function and then return the values"""
     values = []
     for i in range(n):
         values.append(create_task(wait_random(max_delay)))
