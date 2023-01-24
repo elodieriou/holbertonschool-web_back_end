b06879c356d0944cd777c6d56026ac3252a3883d
Elodie Riou <elodie.riou@hotmail.com>
2023-01-24 12:39:56 +0100

    FIX: only use import

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """This module defines the wait_random function."""
-from random import uniform
-from asyncio import sleep
+import random
+import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """This method return a random float between a range with a delay"""
-    delay = uniform(0, max_delay)
-    await sleep(delay)
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
     return delay
