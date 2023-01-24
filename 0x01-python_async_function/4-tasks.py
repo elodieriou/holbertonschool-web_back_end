673c530c54df161be26206e837fc1e7a490b800f
Elodie Riou <elodie.riou@hotmail.com>
2023-01-24 12:36:20 +0100

    FEATURE: implement a task of a task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,7 +3,7 @@
 from asyncio import create_task, as_completed
 from typing import List
 
-task_wait_random = __import__('3-tasks').task_wait_random
+task_wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
