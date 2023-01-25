7b9a436555e0591976a7dceb2ec176510fc08c91
Elodie Riou <elodie.riou@hotmail.com>
2023-01-25 17:12:57 +0100

    FEATURE: implement async comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """This module defines the coroutine async_comprehension."""
-from typing import Generator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """This coroutine collect 10 random numbers using an async comprehensive
     over the method async_generator, and return the 10 random numbers"""
-    return [i async for i in async_generator()]
+    coroutine = [i async for i in async_generator()]
+    return coroutine
