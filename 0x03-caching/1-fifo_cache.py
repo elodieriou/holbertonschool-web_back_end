5c004265e0c5fbbfad28a6b0619194375a0d3673
Elodie Riou <elodie.riou@hotmail.com>
2023-01-26 10:12:28 +0100

    FEATURE: implement FIFO caching

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""FIFOCache module"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache implement method where the item that was added first
+    is the first one to be removed when the cache reaches its maximum
+    capacity.
+
+    This method is approprite for situations where the rate of cache
+    misses is not high and the data being cached is not frequently
+    accessed again"""
+
+    def put(self, key, item):
+        """ Add an item in the cache"""
+        if key is not None or item is not None:
+            self.cache_data[key] = [item]
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_in = next(iter(self.cache_data))
+            self.cache_data.pop(first_in)
+            print("DISCARD: {}".format(first_in))
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
