37da7dc36204fc0fb23c5a655240666622df7aa2
Elodie Riou <elodie.riou@hotmail.com>
2023-01-26 14:28:52 +0100

    FIX: implement a list to store all item update

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -9,16 +9,21 @@ class LIFOCache(BaseCaching):
     capacity.
     """
 
+    def __init__(self):
+        """ Initiliaze"""
+        super().__init__()
+        self.list_of_keys = []
+
     def put(self, key, item):
         """ Add an item in the cache"""
         if key is not None or item is not None:
             self.cache_data[key] = [item]
+            self.list_of_keys.append(key)
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            rev = reversed(self.cache_data)
-            last_in = next(rev) and next(rev)
-            self.cache_data.pop(last_in)
-            print("DISCARD: {}".format(last_in))
+            last_update = self.list_of_keys[-2]
+            self.cache_data.pop(last_update)
+            print("DISCARD: {}".format(last_update))
 
     def get(self, key):
         """Get an item by key"""
