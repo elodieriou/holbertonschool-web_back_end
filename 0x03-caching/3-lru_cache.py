111b08d05f34e7d3aa12e226e2c5ebe2ccb2be56
Elodie Riou <elodie.riou@hotmail.com>
2023-01-26 15:10:58 +0100

    FEATURE: implement LRU caching method

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""LRUCache module"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUCache implement method where the item that has been
+    used least recently is the first one to be removed"""
+
+    def __init__(self):
+        """ Initiliaze"""
+        super().__init__()
+        self.list_of_keys = []
+
+    def put(self, key, item):
+        """ Add an item in the cache"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+            if key not in self.list_of_keys:
+                self.list_of_keys.append(key)
+            else:
+                self.list_of_keys.remove(key)
+                self.list_of_keys.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            least_recently = self.list_of_keys[0]
+            self.cache_data.pop(least_recently)
+            self.list_of_keys.remove(least_recently)
+            print("DISCARD: {}".format(least_recently))
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.list_of_keys.remove(key)
+        self.list_of_keys.append(key)
+        return self.cache_data[key]
