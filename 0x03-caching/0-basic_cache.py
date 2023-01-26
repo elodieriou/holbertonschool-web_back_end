99a1e6b5b55e31bfc5b3ecf4c89908487a653b2b
Elodie Riou <elodie.riou@hotmail.com>
2023-01-26 18:09:21 +0100

    FIX: change item

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -12,7 +12,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """Add an item in the cache"""
         if key is not None or item is not None:
-            self.cache_data[key] = [item]
+            self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key"""
