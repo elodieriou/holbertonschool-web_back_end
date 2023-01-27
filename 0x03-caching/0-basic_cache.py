4743e3793170dd785025690496699c8319675fa2
Elodie Riou <elodie.riou@hotmail.com>
2023-01-27 09:06:01 +0100

    FIX: change or by and

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -11,11 +11,11 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """Add an item in the cache"""
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key"""
-        if key is None or key not in self.cache_data:
+        if key is None and key not in self.cache_data:
             return None
         return self.cache_data[key]
