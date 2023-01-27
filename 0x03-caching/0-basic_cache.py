23898fcbf51a3922c5f79d924e0fbf8edbe7e72a
Elodie Riou <elodie.riou@hotmail.com>
2023-01-27 09:11:25 +0100

    FIX: reverse condition on get method

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -16,6 +16,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """Get an item by key"""
-        if key is None and key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
