4b3bc731e59cbda72ee5944d97d7d7a25dfae1ec
Elodie Riou <elodie.riou@hotmail.com>
2023-01-30 10:29:13 +0100

    FEATURE: implement simple pagination

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,38 @@
+import csv
+import math
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Indexes to paginate the dataset correctly and returns  the
+        appropriate page of the dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        indexes = index_range(page, page_size)
+        start = indexes[0]
+        end = indexes[1]
+
+        if start >= len(self.dataset()):
+            return []
+        return self.dataset()[start:end]
