da9764f002c53848c61414f4399acea38910ae62
Elodie Riou <elodie.riou@hotmail.com>
2023-02-01 10:15:52 +0100

    FIX: add function's comment

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -8,6 +8,7 @@ import re
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
+    """Return the log message obfuscated by 'redaction' argument"""
     for i in fields:
         message = re.sub(i + '=.+?' + separator,
                          i + '=' + redaction + separator,
