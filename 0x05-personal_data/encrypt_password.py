09ebca77c576ec4e884e4a3ddb713c5584257c5e
Elodie Riou <elodie.riou@hotmail.com>
2023-02-02 12:55:38 +0100

    FEATURE: check valid password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -6,14 +6,29 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """THis function hash a password using bcrypt method :
+    """This function hash a password using bcrypt method :
     - Converting password to array of bytes
     - Create a salt, a random data that will be added to the password
      before hashing
     - Hash the password in byte string and add the salt before
+    Return the hashed password
     """
+
     convert = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(convert, salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """This function checks if the password is valid :
+    - Convert en byte the password
+    - Check if the password in bytes is in hashed password
+    Return True if it is the great password, False otherwise
+    """
+
+    convert = password.encode('utf-8')
+    check = bcrypt.checkpw(convert, hashed_password)
+
+    return check
