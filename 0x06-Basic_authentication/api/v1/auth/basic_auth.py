4f073cba504fc3727cfeb8948881bcb79d842106
Elodie Riou <elodie.riou@hotmail.com>
2023-02-06 13:41:04 +0100

    FEATURE: extract base64 of a basic authentication

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,24 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """Empty"""
+    """Class BasicAuth defines the following methods:
+    - extract_base64_authorization_header: returns the Base64 part of
+     the Authorization header for a Basic Authentication
+    """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ This method returns the base64 string of a Basic Authentication
+        :param authorization_header: the authorization header
+        :return: None or base64 string if it is a Basic Authentication
+        """
+
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.split("Basic ")[1]
