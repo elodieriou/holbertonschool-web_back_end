977d8ffbe902ebfa2440c7c86c2ac2232681f50c
Elodie Riou <elodie.riou@hotmail.com>
2023-02-08 10:21:13 +0100

    FEATURE: Create session auth

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module of Session Authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class SessionAuth defines the followings methods:
+    """
