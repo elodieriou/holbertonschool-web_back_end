9aeee19a0716e1473f5be391623ef82c598a44b8
Elodie Riou <elodie.riou@hotmail.com>
2023-02-08 14:44:36 +0100

    FEATURE: update before request for session_cookie

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -56,17 +56,18 @@ def before_request() -> str:
 
     excluded_paths = ['/api/v1/stat*',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
-    if not auth.require_auth(path=request.path,
-                             excluded_paths=excluded_paths):
+    if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request=request) is None:
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
 
-    if auth.current_user(request=request) is None:
+    if auth.current_user(request) is None:
         abort(403)
+
 
     request.current_user = auth.current_user(request)
 
