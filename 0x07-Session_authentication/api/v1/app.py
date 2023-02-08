6fc3c123814d37f3b0d354e0f4845cccdcdf7d03
Elodie Riou <elodie.riou@hotmail.com>
2023-02-08 10:09:58 +0100

    FEATURE: update GET user id

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -65,6 +65,8 @@ def before_request() -> str:
     if auth.current_user(request=request) is None:
         abort(403)
 
+    request.current_user = auth.current_user(request)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
