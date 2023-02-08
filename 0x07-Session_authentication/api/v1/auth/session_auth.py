0fe68b0d95ad0e01017c3ed17056b70a740584e9
Elodie Riou <elodie.riou@hotmail.com>
2023-02-08 11:14:21 +0100

    FEATURE: implement method that create a session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,23 @@
 """ Module of Session Authentication
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Class SessionAuth defines the followings methods:
     """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ This method create a Session ID for a user_id
+        :param user_id: the user ID
+        :return: None or Create a Session ID
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
