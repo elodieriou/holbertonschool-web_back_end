2d931c4201828619ae1e736021a35653f60679cc
Elodie Riou <elodie.riou@hotmail.com>
2023-02-09 09:35:36 +0100

    FIX: pycodestyle

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -62,12 +62,12 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None and\
+            auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
         abort(403)
-
 
     request.current_user = auth.current_user(request)
 
