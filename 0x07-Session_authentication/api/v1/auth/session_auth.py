334e27a5278e2ab178f1c2fd7afa92ad2223333c
Elodie Riou <elodie.riou@hotmail.com>
2023-02-09 10:45:22 +0100

    FIX: retur False when exception

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -65,6 +65,6 @@ class SessionAuth(Auth):
         try:
             del self.user_id_by_session_id[session_id]
         except Exception:
-            pass
+            return False
 
         return True
