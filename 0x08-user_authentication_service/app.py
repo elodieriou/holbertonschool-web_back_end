9adbf9bcfc361893f5b004c143a2e8d46c5fd919
Elodie Riou <elodie.riou@hotmail.com>
2023-02-14 11:03:03 +0100

    FEATURE: end point login

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Route module for the API
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -27,6 +27,25 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /sessions """
+    try:
+        email: str = request.form.get('email')
+        password: str = request.form.get('password')
+    except Exception:
+        return abort(401)
+
+    check_password: bool = AUTH.valid_login(email=email, password=password)
+    if check_password:
+        session_id: str = AUTH.create_session(email=email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
