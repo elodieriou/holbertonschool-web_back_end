ac7f0ebcf816e47ac4b6def3f2867531b75039ef
Elodie Riou <elodie.riou@hotmail.com>
2023-02-15 12:36:30 +0100

    FIX: pycodestyle

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -115,6 +115,8 @@ class Auth:
         try:
             user: User = self._db.find_user_by(reset_token=reset_token)
             hash_pwd = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=hash_pwd, reset_token=None)
+            self._db.update_user(user.id,
+                                 hashed_password=hash_pwd,
+                                 reset_token=None)
         except ValueError:
             raise ValueError
