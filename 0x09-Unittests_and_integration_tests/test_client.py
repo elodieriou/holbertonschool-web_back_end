21a66bd83893a35973cdd95b6f2ccc9c6dd03f68
Elodie Riou <elodie.riou@hotmail.com>
2023-02-21 15:43:33 +0100

    FEATURE: more patching

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -17,8 +17,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_org(self, org_name: str, mock_get) -> None:
         """ Test org method"""
-        mock_json = Mock(return_value={})
-        mock_get.return_value = Mock(json=mock_json)
+        mock_get.return_value = {}
 
         result = GithubOrgClient(org_name=org_name).org
         self.assertEqual(result, mock_get.return_value)
@@ -29,7 +28,24 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_obj = 'client.GithubOrgClient.org'
         mock_value = {'repos_url': 'https://api.github.com/orgs/google'}
 
-        with patch(mock_obj, new_callable=PropertyMock) as mock_repos_url:
-            mock_repos_url.return_value = mock_value
-            result = GithubOrgClient("google").org
+        with patch(mock_obj,
+                   new_callable=PropertyMock,
+                   return_value=mock_value) as mock_repos_url:
+            result = GithubOrgClient("Google").org
             self.assertEqual(result, mock_repos_url.return_value)
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get) -> None:
+        """ Test public_repos """
+        mock_get.return_value = [{'name': 'Google'}]
+
+        mock_obj = 'client.GithubOrgClient._public_repos_url'
+        mock_value = "Google"
+
+        with patch(mock_obj,
+                   new_callable=PropertyMock,
+                   return_value=mock_value) as mock_public_repos:
+            result = GithubOrgClient("Google").public_repos()
+            self.assertEqual(result, ['Google'])
+            mock_get.assert_called_once()
+            mock_public_repos.assert_called_once()
