cf20ef822ee0b0c3ded3fedeaf4b71fc81f1f0fb
Elodie Riou <elodie.riou@hotmail.com>
2023-02-23 16:11:20 +0100

    FEATURE: user user locale

---
+++ 0x0A-i18n/6-app.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+""" Flask app """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Dict
+
+
+app = Flask(__name__)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """ Supported languages list """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Use the locale language """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.before_request
+def before_request() -> None:
+    user = get_user()
+    if user:
+        g.user = user
+
+
+def get_user() -> Dict:
+    """ Retrieve the user thanks its ID """
+    try:
+        user_id = int(request.args.get('login_as'))
+        if user_id in users.keys():
+            return users[user_id]
+    except Exception:
+        return None
+
+
+@app.route("/", methods=['GET'])
+def welcome() -> str:
+    """ Message of Welcome """
+    try:
+        username = g.user['name']
+    except Exception:
+        username = None
+
+    return render_template('6-index.html', username=username)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
