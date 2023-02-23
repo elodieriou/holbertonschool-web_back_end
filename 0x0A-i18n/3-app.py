fd23a4bd443e7517c7c420a66d98178b6c5a6bff
Elodie Riou <elodie.riou@hotmail.com>
2023-02-23 13:45:00 +0100

    FEATURE: parametrize templates

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from typing import Any, Optional
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Supported languages list """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_local() -> Optional[Any]:
+    """ Use the locale language """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=['GET'])
+def welcome() -> str:
+    """ Message of Welcome """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
