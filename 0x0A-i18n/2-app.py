a26cd5832ff80d1c8e7fa2550eaa4008966a7a4a
Elodie Riou <elodie.riou@hotmail.com>
2023-02-23 13:52:48 +0100

    FIX: code

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -26,7 +26,7 @@ def get_local() -> Optional[Any]:
 
 
 @app.route("/", methods=['GET'])
-def welcome() -> str:
+def welcome():
     """ Message of Welcome """
     return render_template('2-index.html')
 
