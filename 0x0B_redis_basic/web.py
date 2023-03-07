e0b198fceacc8ad1e1d956c120cd1ce9d7ca5c74
Elodie Riou <elodie.riou@hotmail.com>
2023-03-07 14:50:44 +0100

    FEATURE: implement expiring web cache and tracker

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Web cache and tracker """
+import redis
+import requests
+from functools import wraps
+from typing import Callable, Any
+
+r = redis.Redis()
+
+
+def tracker(method: Callable) -> Callable:
+    """ Track and cache the result with an expiration time """
+    @wraps(method)
+    def wrapper(url) -> Any:
+        """ Wrapper method """
+        key = f'count:{url}'
+        r.incr(key)
+        r.expire(key, 10)
+        return method(url)
+
+    return wrapper
+
+
+@tracker
+def get_page(url: str) -> str:
+    """ Track how many times a particular URL was accessed """
+    response = requests.get(url)
+    return response.text
