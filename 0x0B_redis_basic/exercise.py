032a452ac510ea5391a3d1c052764aeb08465b5d
Elodie Riou <elodie.riou@hotmail.com>
2023-03-07 11:34:15 +0100

    FIX: get method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -21,7 +21,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
         """ Get data and convert it the desired format """
         data = self._redis.get(key)
         if not data:
