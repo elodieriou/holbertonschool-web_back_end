b40d30b6ac5d7dc299378da6d977e49014145b6e
Elodie Riou <elodie.riou@hotmail.com>
2023-03-08 17:26:44 +0100

    FIX: code

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
+SELECT band_name, COALESCE(split, YEAR(NOW())) - formed AS lifespan
 FROM metal_bands
 WHERE BINARY style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
