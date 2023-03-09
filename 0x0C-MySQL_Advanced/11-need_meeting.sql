e48798bae81d4d018c823fa7248449aad4068914
Elodie Riou <elodie.riou@hotmail.com>
2023-03-09 15:37:52 +0100

    FIX: issuer

--- 0x0C-MySQL_Advanced/11-need_meeting.sql
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -2,5 +2,4 @@
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name FROM students
-WHERE score < 80 AND last_meeting = NULL
-OR last_meeting > DATE_SUB(last_meeting, INTERVAL 1 MONTH);
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(last_meeting, INTERVAL 1 MONTH));
