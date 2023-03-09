94cc45c8a2eb8d1db28ad44033d09e45f5d19113
Elodie Riou <elodie.riou@hotmail.com>
2023-03-09 17:55:05 +0100

    FEATURE: task 13

---
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,26 @@
+-- Creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+BEGIN
+    DECLARE average_weighted FLOAT;
+    DECLARE i INT DEFAULT 0;
+
+    WHILE i < (SELECT COUNT(*) FROM users) DO
+        SET @user_id = (SELECT id FROM users ORDER BY id ASC LIMIT 1 OFFSET i);
+        SET @score = (SELECT score FROM corrections
+            WHERE user_id = @user_id LIMIT 1 OFFSET i);
+        SET @weight = (SELECT weight FROM projects
+            JOIN corrections ON corrections.project_id = projects.id
+            WHERE user_id = @user_id LIMIT 1 OFFSET i);
+
+        IF @score IS NOT NULL AND @weight IS NOT NULL THEN
+            SET average_weighted = @score * @weight;
+            UPDATE users SET average_score = average_weighted WHERE id = @user_id;
+        END IF;
+
+        SET i = i + 1;
+    END WHILE;
+END$$
+DELIMITER ;
+
