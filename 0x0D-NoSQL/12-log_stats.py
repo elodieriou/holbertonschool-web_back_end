61fefb83062a3053fd34b0973976ce93a12e0259
Elodie Riou <elodie.riou@hotmail.com>
2023-03-13 17:55:03 +0100

    FIX: issue

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -6,15 +6,15 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    number_logs = nginx_collection.count()
+    number_logs = nginx_collection.count_documents({})
     print("{} logs".format(number_logs))
 
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        method_document = nginx_collection.find({"method": method})
-        count_method = method_document.count()
-        print("method {}: {}".format(method, count_method))
+        count_method = nginx_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count_method))
 
-    status_check = nginx_collection.find({"method": "GET", "path": "/status"})
-    count_status_check = status_check.count()
+    count_status_check = nginx_collection.count_documents({"method": "GET",
+                                                           "path": "/status"})
     print("{} status check".format(count_status_check))
