e5b3d07ff0dac27b5ae1ede2c99be561fc8d09fb
Elodie Riou <elodie.riou@hotmail.com>
2023-03-17 11:09:50 +0100

    FIX: ESLINT

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -6,7 +6,7 @@ export default function taskBlock(trueOrFalse) {
     const task = true;
     const task2 = false;
 
-    return [task, task2];
+    return [task2, task];
   }
 
   return [task, task2];
