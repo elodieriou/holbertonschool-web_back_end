a4c78cccb3c12bcc48ba9628cb8700611df8939c
Elodie Riou <elodie.riou@hotmail.com>
2023-03-21 11:59:36 +0100

    FIX: issue

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,8 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstname, lastname, fileName) {
-  const promise1 = signUpUser(firstname, lastname);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
 
   return Promise
