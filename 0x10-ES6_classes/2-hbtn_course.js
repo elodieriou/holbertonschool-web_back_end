1abe96b393ea5efd78b1fa74866525f7148c10bb
Elodie Riou <elodie.riou@hotmail.com>
2023-03-22 14:29:43 +0100

    FIX: add throw error on the constructor

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,20 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students) && !students.every((s) => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
