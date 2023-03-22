bd9cb4862af30509d88cfad85dfcf2cac5ac34a1
Elodie Riou <elodie.riou@hotmail.com>
2023-03-22 15:25:57 +0100

    FIX: remove throw error on constructor because the constructor calls the setter

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,20 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-
-    if (!Array.isArray(students) && !students.every((s) => typeof s === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -30,6 +18,7 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
+    console.log('hello');
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,15 +1,7 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-
-    if (typeof name !== 'string') {
-      throw new TypeError('Name mus be a string');
-    }
-
-    this._code = code;
-    this._name = name;
+    this.code = code;
+    this.name = name;
   }
 
   get code() {
