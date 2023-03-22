8f60ff8a17f15c1cbf6a43d9fd8a42db6f9eab63
Elodie Riou <elodie.riou@hotmail.com>
2023-03-22 10:55:49 +0100

    FEATURE: implement building class

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,12 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
