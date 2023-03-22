32f2885a2b83aad4949cf6385a010dcfdc10983a
Elodie Riou <elodie.riou@hotmail.com>
2023-03-22 14:20:06 +0100

    FIX: add condition to verify the type of evacuationWarningMessage

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,6 +1,7 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building) {
+    if (this.constructor !== Building
+    && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
