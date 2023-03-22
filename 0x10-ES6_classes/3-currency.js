1936128386c906f0d567f0c14613233f8acfe944
Elodie Riou <elodie.riou@hotmail.com>
2023-03-22 10:32:47 +0100

    FIX: verify type of attribute during object creation

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -13,10 +13,16 @@ export default class Currency {
   }
 
   set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
     this._code = newCode;
   }
 
   set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name mus be a string');
+    }
     this._name = newName;
   }
 
