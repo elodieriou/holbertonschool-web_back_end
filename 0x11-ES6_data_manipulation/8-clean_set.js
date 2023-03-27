865b9b53279b6daa978dac762c6c45efa4f14b98
Elodie Riou <elodie.riou@hotmail.com>
2023-03-27 15:32:25 +0200

    FEATURE: use more function apply to set

---
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+
+  const startBy = [];
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) {
+      startBy.push(element.slice(startString.length));
+    }
+  });
+
+  return startBy.join('-');
+}
