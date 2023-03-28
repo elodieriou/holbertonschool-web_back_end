ecac1fb893e2bbeae68efdfde90478f1f51c8a4a
Elodie Riou <elodie.riou@hotmail.com>
2023-03-28 09:36:39 +0200

    FIX: check if startstring is type of string

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (startString === '' || startString === undefined) {
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
 
   const startBy = [];
   set.forEach((element) => {
     if (element !== undefined && element.startsWith(startString)) {
-      startBy.push(element.substring(startString.length));
+      startBy.push(element.slice(startString.length));
     }
   });
 
