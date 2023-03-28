891f57eea356d4a21ea7225f6e4bb2febf8bc2d8
Elodie Riou <elodie.riou@hotmail.com>
2023-03-28 09:52:23 +0200

    FEATURE: use weakMap

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ const weakMap = new WeakMap();
 function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
 
-  if (count >= 5) {
+  if (count + 1 >= 5) {
     throw new Error('Endpoint load is high');
   }
 
