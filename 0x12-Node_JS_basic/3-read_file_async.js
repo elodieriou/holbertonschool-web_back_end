a8833748d9ac204784756da0ef6378d4da6e696b
Elodie Riou <elodie.riou@hotmail.com>
2023-03-29 14:41:19 +0200

    FEATURE: read a file asynchronously with node js

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+
+function countStudents(database_file) {
+
+    return new Promise((resolve, reject) => {
+
+        fs.readFile(database_file, 'utf-8', (error, data) => {
+            if (error) {
+                reject(Error('Cannot load the database'));
+            }
+
+            const removeNewLine = data.split('\n');
+            removeNewLine.shift();
+            const students = removeNewLine.filter((element) => element).map((students) => students.split(','));
+            console.log(`Number of students: ${students.length}`);
+
+            const speciality = [];
+            students.forEach((student) => {
+                const elements = student[3];
+                if (!speciality.includes(elements)) {
+                    speciality.push(elements);
+                }
+            });
+
+            const studentsInCS = students.filter((element) => element[3] === speciality[0]);
+            const namesCS = [];
+            studentsInCS.forEach((student) => {
+                namesCS.push(student[0]);
+            });
+            console.log(`Number of students in ${speciality[0]}: ${studentsInCS.length}. List: ${namesCS.join(', ')}`);
+
+            const studentsInSWE = students.filter((element) => element[3] === speciality[1]);
+            const namesSWE = [];
+            studentsInSWE.forEach((student) => {
+                namesSWE.push(student[0]);
+            });
+            console.log(`Number of students in ${speciality[1]}: ${studentsInSWE.length}. List: ${namesSWE.join(', ')}`);
+
+            resolve();
+        });
+    });
+}
+
+module.exports = countStudents;
