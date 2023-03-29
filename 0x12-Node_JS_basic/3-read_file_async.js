e08a2eab05b5578a3326dae8c651c89198be7457
Elodie Riou <elodie.riou@hotmail.com>
2023-03-29 17:41:29 +0200

    FIX: add else

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -7,38 +7,38 @@ function countStudents(database_file) {
         fs.readFile(database_file, 'utf-8', (error, data) => {
             if (error) {
                 reject(Error('Cannot load the database'));
-            }
+            } else {
+                let message = '';
+                const response = [];
 
-            let message = '';
-            const response = [];
-
-            const removeNewLine = data.split('\n');
-            removeNewLine.shift();
-            const students = removeNewLine.filter((element) => element).map((students) => students.split(','));
-            message = `Number of students: ${students.length}`
-            console.log(message);
-            response.push(message);
-
-            const speciality = [];
-            students.forEach((student) => {
-                const elements = student[3];
-                if (!speciality.includes(elements)) {
-                    speciality.push(elements);
-                }
-            });
-
-            speciality.forEach((spe) => {
-                const studentsBySpe = students.filter((element) => element[3] === spe);
-                const namesStudents = [];
-                studentsBySpe.forEach((student) => {
-                    namesStudents.push(student[0]);
-                });
-                message = `Number of students in ${spe}: ${studentsBySpe.length}. List: ${namesStudents.join(', ')}`;
+                const removeNewLine = data.split('\n');
+                removeNewLine.shift();
+                const students = removeNewLine.filter((element) => element).map((students) => students.split(','));
+                message = `Number of students: ${students.length}`
                 console.log(message);
                 response.push(message);
-            });
 
-            resolve(response);
+                const speciality = [];
+                students.forEach((student) => {
+                    const elements = student[3];
+                    if (!speciality.includes(elements)) {
+                        speciality.push(elements);
+                    }
+                });
+
+                speciality.forEach((spe) => {
+                    const studentsBySpe = students.filter((element) => element[3] === spe);
+                    const namesStudents = [];
+                    studentsBySpe.forEach((student) => {
+                        namesStudents.push(student[0]);
+                    });
+                    message = `Number of students in ${spe}: ${studentsBySpe.length}. List: ${namesStudents.join(', ')}`;
+                    console.log(message);
+                    response.push(message);
+                });
+
+                resolve(response);
+            }
         });
     });
 }
