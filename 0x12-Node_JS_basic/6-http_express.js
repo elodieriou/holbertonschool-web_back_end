b0f7fa9adbf0067543d3eece8d79c4242a8e73b1
Elodie Riou <elodie.riou@hotmail.com>
2023-03-30 09:04:55 +0200

    FEATURE: create a small http server using express

---
+++ 0x12-Node_JS_basic/6-http_express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {});
+module.exports = app;
