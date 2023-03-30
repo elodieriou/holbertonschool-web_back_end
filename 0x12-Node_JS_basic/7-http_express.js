1ab86cd5ee2f5c8c2fa33279733f577139d7d8f7
Elodie Riou <elodie.riou@hotmail.com>
2023-03-30 09:31:16 +0200

    FEATURE: create a more complex http server using express

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+const process = require('process');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+const file = process.argv[2];
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('Hello Holberton School!');
+});
+
+app.get('/students', (request, response) => {
+    response.write('This is the list of our students\n');
+    countStudents(file)
+        .then((data) => {
+            response.end(`${data.join('\n')}`);
+        })
+        .catch((error) => {
+            response.end(`${error.name}: ${error.message}`);
+        });
+});
+
+
+app.listen(port, hostname);
+module.exports = app;
