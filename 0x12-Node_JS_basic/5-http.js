2d413502afa96f1b8780c87de9cd8cc4e34d1201
Elodie Riou <elodie.riou@hotmail.com>
2023-03-30 08:59:19 +0200

    FIX: call function directly

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -4,27 +4,27 @@ const process = require('process');
 
 const hostname = '127.0.0.1';
 const port = 1245;
-const data = process.argv[2];
+const file = process.argv[2];
 
-const app = http.createServer( async (request, response) => {
+const app = http.createServer(  (request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
 
     const url = request.url;
 
     if (url === '/') {
-        response.write('Hello Holberton School!');
+        response.end('Hello Holberton School!');
     } else if (url === '/students') {
         response.write('This is the list of our students\n');
-        try {
-            const students = await countStudents(data);
-            response.end(`${students.join('\n')}`);
-        } catch (error) {
-            response.end(`${error.name}: ${error.message}`);
-        }
+        countStudents(file)
+            .then((data) => {
+            response.end(`${data.join('\n')}`);
+            })
+            .catch((error) => {
+                response.end(`${error.name}: ${error.message}`);
+            });
     }
-    response.end();
 });
 
-app.listen(port, hostname, () => {});
+app.listen(port, hostname);
 module.exports = app;
