330b22f9063044509a9eb1c342b79f320b8eb76b
Elodie Riou <elodie.riou@hotmail.com>
2023-04-01 20:20:20 +0200

    FIX: issue

--- 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -10,29 +10,26 @@ class StudentsController {
         Object.entries(data).sort().forEach(([key, values]) => {
           studentsBySpe.push(`Number of students in ${key}: ${values.length}. List: ${values.join(', ')}`);
         });
-        response.status(200);
-        response.end(`${studentsBySpe.join('\n')}`);
+        response.status(200).end(`${studentsBySpe.join('\n')}`);
+        response.send();
       })
       .catch((error) => {
-        response.status(500);
-        response.end(error.message);
+        response.status(500).send(error.message);
       });
   }
 
   static getAllStudentsByMajor(request, response, file) {
     const { major } = request.params;
     if (!major.includes('CS') && !major.includes('SWE')) {
-      response.status(500);
-      response.end('Major parameter must be CS or SWE');
+      response.status(500).send('Major parameter must be CS or SWE');
     }
     readDatabase(file)
       .then((data) => {
-        response.status(200);
-        response.end(`List: ${data[major].join(', ')}`);
+        response.status(200).end(`List: ${data[major].join(', ')}`);
+        response.send();
       })
       .catch((error) => {
-        response.status(500);
-        response.end(error.message);
+        response.status(500).send(error.message);
       });
   }
 }
