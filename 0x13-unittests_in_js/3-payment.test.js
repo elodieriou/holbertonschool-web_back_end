b54eb850c5dc1a124a11bd3fe234e2590775915f
Elodie Riou <elodie.riou@hotmail.com>
2023-04-04 18:17:50 +0200

    FEATURE: spy a function

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -1,6 +1,9 @@
 const sinon = require('sinon');
+const chai = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
+
+const { expect } = chai;
 
 describe('sendPaymentRequestToApi', () => {
   it('should call the Utils.calculateNumber with the correct parameters', () => {
@@ -10,6 +13,15 @@ describe('sendPaymentRequestToApi', () => {
 
     sinon.assert.calledOnce(calculateNumberSpy);
     sinon.assert.calledWith(calculateNumberSpy, 'SUM', 100, 20);
+
+    calculateNumberSpy.restore();
+  });
+
+  it('should call the Utils.calculateNumber and result is equal to call sendPaymentRequestToApi', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const sendPaymentRequestToApiSpy = sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy('SUM', 100, 20)).equal(sendPaymentRequestToApiSpy);
 
     calculateNumberSpy.restore();
   });
