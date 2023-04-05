42517d5eca2d4f08ad09f51e402b1499457908c2
Elodie Riou <elodie.riou@hotmail.com>
2023-04-05 12:11:26 +0200

    FIX: refactoring code

--- 0x13-unittests_in_js/2-calcul_chai.js
+++ 0x13-unittests_in_js/2-calcul_chai.js
@@ -3,19 +3,16 @@ function calculateNumber(type, a, b) {
     throw new Error('Missing arguments');
   }
 
-  let arg1 = Number(a);
-  let arg2 = Number(b);
-
-  if (Number.isNaN(arg1) || Number.isNaN(arg2)) {
-    throw new TypeError('The a and b parameters must be integers');
-  }
-
   if (typeof type !== 'string') {
     throw new TypeError('The type parameter must be a string');
   }
 
-  arg1 = Math.round(arg1);
-  arg2 = Math.round(arg2);
+  const arg1 = Math.round(a);
+  const arg2 = Math.round(b);
+
+  if (Number.isNaN(arg1) || Number.isNaN(arg2)) {
+    throw new TypeError('The a and b parameters must be integers');
+  }
 
   switch (type) {
     case 'SUM':
