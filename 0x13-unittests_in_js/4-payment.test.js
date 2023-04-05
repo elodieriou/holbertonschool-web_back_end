75a0cc350f1cac9f9af83b981dc33f067dac3675
Elodie Riou <elodie.riou@hotmail.com>
2023-04-05 13:05:58 +0200

    FIX: tests dans return function

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -1,8 +1,22 @@
 const sinon = require('sinon');
+const chai = require('chai');
 const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
+const { expect } = chai;
+
 describe('sendPaymentRequestToApi', () => {
+  it('should call the Utils.calculateNumber and result is equal to call sendPaymentRequestToApi', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const expected = Utils.calculateNumber('SUM', 100, 20);
+    const actual = sendPaymentRequestToApi(100, 20);
+
+    expect(actual).equal(expected);
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).equal(true);
+
+    calculateNumberSpy.restore();
+  });
+
   it('should call the Utils.calculateNumber with the correct parameters', () => {
     const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
 
