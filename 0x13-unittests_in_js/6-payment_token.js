0323314e989adce36abb6114391ebc622006e432
Elodie Riou <elodie.riou@hotmail.com>
2023-04-05 10:29:24 +0200

    FEATURE: tests async function

---
+++ 0x13-unittests_in_js/6-payment_token.js
@@ -0,0 +1,8 @@
+function getPaymentTokenFromAPI(success) {
+  if (success) {
+    return Promise.resolve({ data: 'Successful response from the API' });
+  }
+  return Promise;
+}
+
+module.exports = getPaymentTokenFromAPI;
