d31f0a29e0a9217e8005d683885e99506263f8de
Elodie Riou <elodie.riou@hotmail.com>
2023-04-05 13:44:46 +0200

    FIX: tests

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -6,19 +6,15 @@ const { expect } = chai;
 describe('basic integration testing', () => {
   it('should return a statuscode 200', (done) => {
     request('http://localhost:7865', (error, response) => {
-      if (response) {
-        expect(response.statusCode).equal(200);
-        done();
-      }
+      expect(response.statusCode).equal(200);
+      done();
     });
   });
 
-  it('should return the body', () => (done) => {
+  it('should return the body', (done) => {
     request('http://localhost:7865', (error, response, body) => {
-      if (response) {
-        expect(body).equal('Welcome to the payment system');
-        done();
-      }
+      expect(body).equal('Welcome to the payment system');
+      done();
     });
   });
 });
