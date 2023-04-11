98044bb143d93231dc618e57e4f1cc0f5fe3beb3
Elodie Riou <elodie.riou@hotmail.com>
2023-04-11 10:21:53 +0200

    FEATURE: node redis client and async operation

---
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+import util from 'util';
+
+const client = redis.createClient();
+const getAsync = util.promisify(client.get).bind(client);
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const reply = await getAsync(schoolName);
+  console.log(reply);
+}
+
+async function callFunctions() {
+  await displaySchoolValue('Holberton');
+  await setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}
+
+callFunctions();
