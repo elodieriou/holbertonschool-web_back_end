df0aa46b803aa35047f5cbdc0e3f79a582d3a479
Elodie Riou <elodie.riou@hotmail.com>
2023-04-11 10:53:53 +0200

    FEATURE: node redis publisher and subscriber

---
+++ 0x14-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,23 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+const CHANNEL = 'holberton school channel';
+client.subscribe(CHANNEL);
+client.on('message', (channel, message) => {
+  if (channel === CHANNEL) {
+    console.log(message);
+  }
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
